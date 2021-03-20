c87b1b476ce085d860f6581ed7d328943b0610a3
ricyanez <78868635+ricyanez@users.noreply.github.com>
2021-03-20 11:57:32 -0700

    Add Request workflow
    Add login and request workflow

--- ApprovalIntelogix/Controllers/LoginController.cs
+++ ApprovalIntelogix/Controllers/LoginController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LoginWs.Controllers
+namespace ApprovalIntelogix.Controllers
 {
     [ApiController]
     [Authorize]
